0ebc789dfc82f59bb94085cd1d718a94aec09401
qinliyang <1093065897@qq.com>
2025-05-12 08:35:16 +0800

    update lint_staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Button } from 'antd'
 
-function App(): JSX.Element {
+function App(): React.JSX.Element {
   return (
     <div className="App">
       <h1>Vite-React-App</h1>
-      <Button type="primary">Button</Button>
+      <Button type="primary">Button_1</Button>
     </div>
   )
 }
